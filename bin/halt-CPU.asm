24c5c0714ddba81c690145f3da4265cd2af2e66f
Michael Goodwin <michael.cgdwn@gmail.com>
2024-07-28 15:26:27 +0100

    Add comments and attribution to halt-CPU.asm

--- bin/halt-CPU.asm
+++ bin/halt-CPU.asm
@@ -1,9 +1,15 @@
-BITS 16
-org 0x7c00
+;
+;  File:    halt-CPU.asm
+;  Author:  Michael Goodwin (NetMasterMichael)
+;  Date:    2024-07-27
+;
+
+[BITS 16]    ; Directive to assembler that we are operating in 16-bit real mode
+org 0x7c00   ; Offset to start program at memory address 0x7C00
 
 start:
-  hlt
-  jmp start
+  hlt        ; Halt CPU momentarily
+  jmp start  ; Jump to the label start
 
-times 510 - ($-$$) db 0
-dw 0xAA55
+times 510 - ($-$$) db 0  ; Pad the remainder of the binary with zeros
+dw 0xAA55                ; Magic byte that indicates this boot sector is bootable
